c2ef9c1cce81be896fc1bb4fd4fc001805788c02
suraj7975 <167083443+suraj7975@users.noreply.github.com>
2024-04-29 12:42:18 +0530

    Create Insertion-Sort Algorithm.py
    Insertion-Sort Algorithm

---
+++ Insertion-Sort Algorithm.py
@@ -0,0 +1,15 @@
+def insertion_Sort(arr):
+    for i in range(1 , len(arr)):
+        key = arr[i]
+        j =  i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key 
+            
+n = int(input())
+arr = list(map( int , input().split()))
+
+insertion_Sort(arr)
+
+print(*arr)
