8934b909f60c77aee5c78d700faaa59d6b084259
suraj7975 <167083443+suraj7975@users.noreply.github.com>
2024-04-29 12:41:43 +0530

    Create Selection-Sort Algorithm.py
    Selection-Sort Algorithm

---
+++ Selection-Sort Algorithm.py
@@ -0,0 +1,18 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+
+selection_sort(numbers)
+
+
+print(*numbers)
